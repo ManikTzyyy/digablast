32638de20542ad5115aac4530bffe54b29be373f
ahlaiptek <iptekahla@gmail.com>
2025-07-02 23:20:40 +0700

    feat: add navbar component and integrate it into home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,33 +11,7 @@
 </head>
 
 <body>
-    <div id="navbar" class="fixed top-0 z-50 w-full">
-        <div class="flex justify-between text-white">
-            <a href="/"><h1 class="text-2xl font-medium p-6">DigiBlast</h1></a>
-            <div class="hidden sm:flex items-center gap-6 sm:px-4">
-                <a href="#section-about">About</a>
-                <a href="#section-pricing">Pricing</a>
-                <a href="#footer">Contact</a>
-                <div class="flex gap-2"><a href="/login" class="button button-success">Sign In</a>
-                    <a href="/register" class="button button-transparent">Sign Up</a>
-                </div>
-            </div>
-            <button id="menu-bars" class=" p-6 relative z-10 text-3xl top-0 sm:hidden"><i
-                    class="fa-solid fa-bars icon-bar"></i></button>
-            <div id="mobile-menu" class="sm:hidden flex absolute bg-white text-black w-full h-screen">
-                <div class="flex flex-col items-start gap-6 absolute p-7">
-                    <div class="flex gap-5"><a href="/login" class="button button-success">Sign In</a>
-                        <a href="/register" class="button button-transparent">Sign Up</a>
-                    </div>
-                    <a href="#section-about">About</a>
-                    <a href="#section-pricing">Pricing</a>
-                    <a href="#footer">Contact</a>
-
-                </div>
-            </div>
-
-        </div>
-    </div>
+    @include('components.navbar')
     <div id="hero-section"
         class="relative overflow-hidden bg-gradient-to-t text-white from-black to-primary min-h-screen flex sm:flex-nowrap justify-between items-center gap-2 sm:pt-10">
         <div class="cube -z-0 rotate-45 top-0 -left-44"></div>
